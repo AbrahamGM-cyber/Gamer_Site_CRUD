9e25f552edf72dda111a5d72e2ce89d0e5e2b12a
AbrahamGM-cyber <agonzalezmora2@gmail.com>
2023-06-03 10:07:33 -0400

    conexion con host de clever cloud

--- src/controllers/Game_Controller.js
+++ src/controllers/Game_Controller.js
@@ -2,7 +2,18 @@ const controller = {};
 //home
 controller.home = (req, res) => { res.render('main.ejs') };
 //catalogo
-controller.catalogo = (req, res) => { res.render('catalogo.ejs') };
+controller.catalogo = (req, res) => {
+    req.getConnection((err, conn) => {
+        conn.query('SELECT * FROM Juegos', (err, Juegos) => {
+            if (err) {
+                res.json(err);
+            }
+            res.render('catalogo.ejs', {
+                data: Juegos
+            });
+        });
+    });
+};
 
 //about
 controller.about = (req, res) => { res.render('about.ejs') };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const path = require('path');
 const morgan = require('morgan');
-const exphbs = require('express-handlebars');
-const path = require('path')
+const mysql = require('mysql');
+const myconnection = require('express-myconnection');
+const cookieParser = require('cookie-parser')
 
 //iniciarlizarse
 const app = express();
+const { urlencoded } = require('express');
 
 //Settings
 app.set('port', process.env.PORT || 8080);
@@ -13,6 +16,17 @@ app.set('views', path.join(__dirname, 'views'))
 
 //Middlewares
 app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(express.json());
+app.use(myconnection(mysql, {
+    host: 'bnlaaox1boalnkpet9bx-mysql.services.clever-cloud.com',
+    user: 'uvqbbmuh71wargnx',
+    password: 'mnyh2R5ydFputN00kqNs',
+    port: 3306,
+    database: 'bnlaaox1boalnkpet9bx'
+}, 'single'));
+
+app.use(express.urlencoded({ extended: false }));
 
 //Routes
 app.use(require('./routes'));
